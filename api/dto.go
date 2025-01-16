16564edd81b2f0a5b7bd60975f4b68f4dec3321d
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-01-16 19:08:42 +0700

    [docs] add comments and tags

--- api/dto.go
+++ api/dto.go
@@ -3,16 +3,16 @@ package api
 import "time"
 
 type CreateLink struct {
-	TargetURL string `json:"targetUrl" validate:"required"`
+	TargetURL string `json:"targetUrl" validate:"required,http_url"` // Target link to shorten
 }
 
 type Link struct {
-	ID        string `json:"id"`
-	TargetURL string `json:"targetUrl"`
-	URL       string `json:"url"`
+	ID        string `json:"id"`                     // ID
+	TargetURL string `json:"targetUrl" format:"uri"` // Target link
+	URL       string `json:"url" format:"uri"`       // Short link
 
-	CreatedAt  time.Time `json:"createdAt"`
-	ValidUntil time.Time `json:"validUntil"`
+	CreatedAt  time.Time `json:"createdAt" format:"date-time"`  // Created at
+	ValidUntil time.Time `json:"validUntil" format:"date-time"` // Valid until
 }
 
 type GetLinkResponse struct {
@@ -27,10 +27,9 @@ type PostLinksResponse struct {
 	Link Link `json:"link"`
 }
 
-// Stats
 type Stats struct {
-	Labels map[string]map[string]int `json:"labels"`
-	Total  int                       `json:"total"`
+	Labels map[string]map[string]int `json:"labels"` // Redirects by labels' values
+	Total  int                       `json:"total"`  // Total redirects
 }
 
 type GetStatsResponse struct {
